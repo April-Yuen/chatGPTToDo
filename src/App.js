d101827987dd06e23f215cca178497ad5099e3c6
April Yuen <april.you@gmail.com>
2023-02-12 19:21:11 -0500

    Adding first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Learn React.js" },
+    { id: 2, text: "Build a to-do list app" },
+    { id: 3, text: "Publish the app" },
+  ]);
+
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { id: todos.length + 1, text }];
+    setTodos(newTodos);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(newTodo);
+    setNewTodo("");
+  };
+
+  const removeTodo = (id) => {
+    const newTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+        crossorigin="anonymous"
+      />
+      <div className="container">
+        <h1 className="text-center my-5">My To-Do List</h1>
+        <ul className="list-group">
+          {todos.map((todo) => (
+            <li key={todo.id} className="list-group-item">
+              {todo.text}{" "}
+              <button
+                className="btn btn-danger btn-sm float-right"
+                onClick={() => removeTodo(todo.id)}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+        <form className="my-5" onSubmit={handleSubmit}>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Add a new to-do"
+              value={newTodo}
+              onChange={(e) => setNewTodo(e.target.value)}
+            />
+          </div>
+          <button className="btn btn-primary">Add To-Do</button>
+        </form>
+      </div>
+    </>
   );
-}
+};
 
-export default App;
+export default TodoList;
+
